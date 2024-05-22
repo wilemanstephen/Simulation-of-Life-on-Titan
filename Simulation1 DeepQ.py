a585fca98709db5fe7021bb1e16e6907cb9450a5
wilemanstephen <mateiwileman@gmail.com>
2024-05-22 14:48:42 +0200

    s
    s

--- Simulation1 DeepQ.py
+++ Simulation1 DeepQ.py
@@ -2,6 +2,7 @@ import os
 import sys
 import io
 import cv2
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
@@ -16,17 +17,17 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 style.use("ggplot")
 
 SIZE = 20
-EPISODES = 1000
+EPISODES = 10000
 MOVE_PENALTY = 1
 MOUNTAIN_REWARD = 300
 EPSILON = 0.9
 EPSILON_DECAY = 0.99975
-SHOW_EVERY = 100
+MIN_EPSILON = 0.01
 LEARNING_RATE = 0.001
 DISCOUNT = 0.95
-TARGET = (SIZE - 1, SIZE // 2)
-CHARACTER_1 = 1
-PASTEL = {1: (0, 255, 0)}
+MINIBATCH_SIZE = 64
+MIN_REPLAY_MEMORY_SIZE = 1000
+SHOW_EVERY = 100
 
 DIRECTORY = "Data S1 DeepQ"
 if not os.path.exists(DIRECTORY):
@@ -45,24 +46,49 @@ def next_run_number(directory):
 run_number = next_run_number(DIRECTORY)
 filename = f"{DIRECTORY}/Episode_rewards_{run_number}.jpg"
 
-class AGENT:
+class DQNAgent:
     def __init__(self):
-        self.x = 0
-        self.y = SIZE - 1
+        self.model = self.create_model()
+        self.target_model = self.create_model()
+        self.target_model.set_weights(self.model.get_weights())
+        self.replay_memory = deque(maxlen=20000)
 
-    def action(self, choice):
-        if choice == 0:
-            self.move(1, 0)
-        elif choice == 1:
-            self.move(-1, 0)
-        elif choice == 2:
-            self.move(0, 1)
-        elif choice == 3:
-            self.move(0, -1)
+    def create_model(self):
+        model = tf.keras.Sequential([
+            tf.keras.layers.Dense(256, input_shape=(SIZE,), activation='relu'),
+            tf.keras.layers.Dense(256, activation='relu'),
+            tf.keras.layers.Dense(4, activation='linear')
+        ])
+        model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE))
+        return model
 
-    def move(self, x, y):
-        self.x = np.clip(self.x + x, 0, SIZE - 1)
-        self.y = np.clip(self.y + y, 0, SIZE - 1)
+    def update_replay_memory(self, transition):
+        self.replay_memory.append(transition)
+
+    def train(self, terminal_state):
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            return
+        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+        current_states = np.array([transition[0] for transition in minibatch])
+        current_qs_list = self.model.predict(current_states)
+        new_current_states = np.array([transition[3] for transition in minibatch])
+        future_qs_list = self.target_model.predict(new_current_states)
+        X = []
+        y = []
+        for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
+            if not done:
+                max_future_q = np.max(future_qs_list[index])
+                new_q = reward + DISCOUNT * max_future_q
+            else:
+                new_q = reward
+            current_qs = current_qs_list[index]
+            current_qs[action] = new_q
+            X.append(current_state)
+            y.append(current_qs)
+        self.model.fit(np.array(X), np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False if terminal_state else None)
+
+    def get_qs(self, state):
+        return self.model.predict(np.array(state).reshape(-1, *state.shape))[0]
 
 def mountain(size):
     mountain = np.zeros((size, size))
@@ -77,53 +103,37 @@ def mountain(size):
 
 mountain_render = mountain(SIZE)
 
-def mountain_collision(character_x, character_y, mountain_grid):
-    return mountain_grid[character_y, character_x] > 0
-
 def check_if_reached_target(character_x, character_y):
-    return mountain_collision(character_x, character_y, mountain_render)
+    return mountain_render[character_y, character_x] == 1
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Input(shape=(2,)),
-    tf.keras.layers.Dense(24, activation='relu'),
-    tf.keras.layers.Dense(24, activation='relu'),
-    tf.keras.layers.Dense(4, activation='linear')
-])
-model.compile(optimizer='adam', loss='mse')
-
-target_model = tf.keras.models.clone_model(model)
-target_model.set_weights(model.get_weights())
-replay_memory = deque(maxlen=20000)
-
+agent = DQNAgent()
 episode_rewards = []
 for episode in tqdm(range(EPISODES), ascii=True, unit='episodes'):
-    character = AGENT()
     episode_reward = 0
-    for i in range(200):
-        obs = np.array([character.x - TARGET[0], character.y - TARGET[1]])
+    current_state = np.random.randint(0, SIZE, (SIZE,))
+    done = False
+
+    while not done:
         if np.random.random() > EPSILON:
-            action = np.argmax(model.predict(obs.reshape(1, -1))[0])
+            action = np.argmax(agent.get_qs(current_state))
         else:
             action = np.random.randint(0, 4)
-        character.action(action)
-        reward = MOUNTAIN_REWARD if mountain_collision(character.x, character.y, mountain_render) else -MOVE_PENALTY
-        done = check_if_reached_target(character.x, character.y)
+        new_state = current_state.copy()
+        if action == 1:
+            reward = MOUNTAIN_REWARD if check_if_reached_target(current_state[0], current_state[1]) else -MOVE_PENALTY
+            done = True
+        else:
+            reward = -MOVE_PENALTY
         episode_reward += reward
-        if done:
-            break
-        if episode % SHOW_EVERY == 0:
-            env = np.zeros((SIZE, SIZE, 3), dtype=np.uint8)
-            env[:] = [120, 120, 120]
-            env[mountain_render == 1] = (0, 0, 255)
-            env[character.y, character.x] = PASTEL[CHARACTER_1]
-            env = cv2.resize(env, (300, 300), interpolation=cv2.INTER_NEAREST)
-            cv2.imshow('Game', env)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-    EPSILON *= EPSILON_DECAY
-    episode_rewards.append(episode_reward)
+        agent.update_replay_memory((current_state, action, reward, new_state, done))
+        agent.train(done)
+        current_state = new_state
 
-cv2.destroyAllWindows()
+    episode_rewards.append(episode_reward)
+    if EPSILON > MIN_EPSILON:
+        EPSILON *= EPSILON_DECAY
+    else:
+        EPSILON = MIN_EPSILON
 
 reward_array = np.array(episode_rewards)
 smoothed_rewards = np.convolve(reward_array, np.ones((SHOW_EVERY,))/SHOW_EVERY, mode='valid')
